e131149c69224f7b3e357493a7f2d8a5cc9560b2
Tanko Hunor <tanko.hunor.a@gmail.com>
2021-01-29 12:42:13 +0000

    Chsnged barcode reading mode

--- test.py
+++ test.py
@@ -1,35 +1,15 @@
-#!/home/pi/tt_barcode_validator/venv/bin/python
-
 import RPi.GPIO as GPIO
 import os
+import sys
 from datetime import datetime
 import time
 from csv import reader
-from evdev import *
-from timeout import timeout, TimeoutError
+from inputimeout import inputimeout, TimeoutOccurred
 
 dirname = os.path.dirname(__file__)
 ean_csv_file_path = os.path.join(dirname, 'ean_list.csv')
 validated_products_csv_path = os.path.join(dirname, 'validated_products.csv')
 
-@timeout(3)
-def read_barcode():
-    dev = InputDevice('/dev/input/event4')
-    x = ''
-
-    scancodes = {2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 8: u'7', 9: u'8',
-        10: u'9', 11: u'0'}
-
-    for event in dev.read_loop():
-        if event.type == ecodes.EV_KEY:
-            data = categorize(event)
-            # Filter out only down events
-            if data.keystate == 1:
-                key_lookup = u'{}'.format(scancodes.get(data.scancode)) or u'UNKNOWN:[{}]'.format(data.scancode)
-                if (data.scancode == 28):
-                    return(x)
-                else:
-                    x += key_lookup
 
 def list_from_csv(path, col_nr):
     # returns a list of integers from the nth column (starting from 0) of a csv file
@@ -77,22 +57,21 @@ def validate_ean13(code):
     else:
         return False
 
-def read_barcodes(a=0):
+def read_barcode(a=0):
     print('trying to read code')
+#    try:
+#        read_ean_13 = inputimeout(prompt='Reading EAN13 barcode', timeout=3)
+#        if not validate_ean13(read_ean_13):
+#           raise(TimeoutOccurred)
+#   except TimeoutOccurred:
+#       set_relays_to_error()
+#       return False
     try:
-        read_ean_13 = read_barcode()
-        print(read_ean_13)
-        if not validate_ean13(read_ean_13):
-            raise(TimeoutError)
-    except TimeoutError:
-        set_relays_to_error()
-        return False
-    try:
-        read_code_39 = read_barcode()
-        print(read_code_39)
+        #read_code_39 = inputimeout(prompt='Reading CODE39 barcode', timeout=3)
+        read_code_39 = input('reading code 39')
         if not validate_code39(read_code_39):
-            raise(TimeoutError)
-    except TimeoutError:
+            raise(TimeoutOccurred)
+    except TimeoutOccurred:
         set_relays_to_error()
         return False
     # If correct reading does happen in 2 seconds
@@ -104,6 +83,7 @@ def read_barcodes(a=0):
 def write_to_file(code_39, ean_13):
     timestamp = datetime.now(tz=None)
     new_line = '{},{},{}'.format(timestamp, code_39, ean_13)
+    print(new_line, flush=True)
     with open(validated_products_csv_path, 'a+') as fi:
         # Move read cursor to the start of the file
         fi.seek(0)
@@ -111,28 +91,27 @@ def write_to_file(code_39, ean_13):
         data = fi.read(100)
         if len(data) > 0:
             fi.write('\n')
-            # Append new line to end
+        # Append new line to end
         fi.write(new_line)
         
 def loop():
     while True:        
         # Wait for sensor trigger
-        #GPIO.add_event_detect(1, GPIO.RISING)
         if GPIO.input(1) == 0:
-            print("triggered")
-            read_barcodes()
+            print("triggered", flush=True)
+            read_barcode()
         if GPIO.input(24) == 1:
-            print('RESET')
+            print('RESET', flush=True)
             set_relays_to_normal()
         time.sleep(0.2)
-        #GPIO.add_event_callback(1, test_trigger)
     
-
-
 if __name__ == '__main__':
     setup()
     try:
-        loop()
+        i = sys.stdin.readline()
+        print(i)
+        #loop()
+
     except KeyboardInterrupt:
         print("Interrupted")
         GPIO.cleanup()
